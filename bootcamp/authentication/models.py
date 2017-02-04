38966cfe4db8ad50db9fdaa1c50f448b01a9c3e9
jinlixin <444989527@qq.com>
2017-02-04 23:14:47 +0800

    将google字体等本地化，加快网页响应速度

--- bootcamp/authentication/models.py
+++ bootcamp/authentication/models.py
@@ -34,23 +34,14 @@ class Profile(models.Model):
         return url
 
     def get_picture(self):
-        no_picture = 'http://trybootcamp.vitorfs.com/static/img/user.png'
-        try:
-            filename = settings.MEDIA_ROOT + '/profile_pictures/' +\
-                self.user.username + '.jpg'
-            picture_url = settings.MEDIA_URL + 'profile_pictures/' +\
-                self.user.username + '.jpg'
-            if os.path.isfile(filename):
-                return picture_url
-            else:
-                gravatar_url = 'http://www.gravatar.com/avatar/{0}?{1}'.format(
-                    hashlib.md5(self.user.email.lower()).hexdigest(),
-                    urllib.urlencode({'d': no_picture, 's': '256'})
-                    )
-                return gravatar_url
-
-        except Exception:
-            return no_picture
+        filename = settings.MEDIA_ROOT + '/profile_pictures/' +\
+            self.user.username + '.jpg'
+        picture_url = settings.MEDIA_URL + 'profile_pictures/' +\
+            self.user.username + '.jpg'
+        if os.path.isfile(filename):
+            return picture_url
+        else:
+            return settings.MEDIA_URL + 'profile_pictures/user.png'
 
     def get_screen_name(self):
         try:
